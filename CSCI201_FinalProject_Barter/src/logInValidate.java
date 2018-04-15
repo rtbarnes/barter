8465a5acaf2a02988aec8a77b40d2fe2c5966520
srishti1 <srishtim@usc.edu>
2018-04-15 16:02:38 -0700

    updated logINvalidate

--- CSCI201_FinalProject_Barter/src/logInValidate.java
+++ CSCI201_FinalProject_Barter/src/logInValidate.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -53,6 +54,8 @@ public class logInValidate extends HttpServlet {
 					System.out.println("wrong password");
 				}
 				else{
+					HttpSession session = request.getSession();
+					session.setAttribute("user", UserObject);					
 					pageTo = "/itemResults.html";
 					System.out.println("success");
 				}
