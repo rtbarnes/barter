14fa501aa29b6c364bd3dbdead3910209a30056a
rtbarnes <rtbarnes@usc.edu>
2018-04-18 22:44:22 -0700

    Individual item page populated correctly, itemResults links to unique itemPages -Reed

---
+++ 201_final_barter/src/servlets/DisplaySingleItem.java
@@ -0,0 +1,36 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import helpers.Util;
+import model.Item;
+import model.User;
+
+@WebServlet("/DisplaySingleItem")
+public class DisplaySingleItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int itemId = Integer.parseInt(request.getParameter("itemId"));
+		
+		Util util = new Util();
+		Item item = util.getItemByItemId(itemId);
+		User user = util.getUserByUserId(item.getUserId());
+		
+		request.setAttribute("item", item);
+		request.setAttribute("seller", user);
+
+    	String pageTo = "/itemPage.jsp?itemId="+itemId;
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(pageTo);
+		dispatcher.forward(request, response);
+	}
+
+}
