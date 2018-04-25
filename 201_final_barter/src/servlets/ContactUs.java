d0ea09e9d2d6ea8df75d608e8119be782842aa9f
Qingshen Huang <qingsheh@usc.edu>
2018-04-24 22:47:50 -0700

    almost final

--- 201_final_barter/src/servlets/ContactUs.java
+++ 201_final_barter/src/servlets/ContactUs.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ContactUs
  */
+
 @WebServlet("/ContactUs")
 public class ContactUs extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -17,8 +18,15 @@ public class ContactUs extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String firstName = (String) request.getParameter("firstName");
 		String lastName = (String) request.getParameter("lastName");
+		String email = (String) request.getParameter("email");
+		String inquiry = (String) request.getParameter("inquiry");
+		
 		System.out.println(firstName);
 		System.out.println(lastName);
+		System.out.println(email);
+		System.out.println(inquiry);
+		
+		
 	}
 
 }
